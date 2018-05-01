3718d1918f21b732327667b9e429bdff0ab03399
Bill Tihen <btihen@gmail.com>
2018-05-01 10:40:34 +0200

    updating look and feel - data & - learn should have the same look and feel

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   mount Spina::Engine => '/learn'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount Lets::Engine => "/lets", as: 'lets'
+  mount Lets::Engine => "/data", as: 'lets'
 end
