e51253e5e0015f9e03a63fa50bd90173783846ad
Bill Tihen <btihen@gmail.com>
2018-05-01 20:55:23 +0200

    finalize landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'static/home'
 
-  mount Spina::Engine => '/learn'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Lets::Engine => "/data", as: 'lets'
+  mount Spina::Engine => '/learn'
 
   root to: 'static#home'
 end
