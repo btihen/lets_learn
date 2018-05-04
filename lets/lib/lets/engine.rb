eb8cd2038190d850f51e4a3f960a10c33ee3993d
Bill Tihen <btihen@gmail.com>
2018-05-04 09:12:44 +0200

    remove devise signup

--- lets/lib/lets/engine.rb
+++ lets/lib/lets/engine.rb
@@ -1,5 +1,16 @@
 module Lets
   class Engine < ::Rails::Engine
+
     isolate_namespace Lets
+
+    # set up for migrations
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match(root.to_s)
+        config.paths["db/migrate"].expanded.each do |p|
+          app.config.paths["db/migrate"] << p
+        end
+      end
+    end
+
   end
 end
