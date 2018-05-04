399e1605734a11bab8429f82e7f5adeebc7f40d2
Bill Tihen <btihen@gmail.com>
2018-05-04 09:13:05 +0200

    remove devise signup

--- lets/app/models/lets/manager.rb
+++ lets/app/models/lets/manager.rb
@@ -2,7 +2,7 @@ module Lets
   class Manager < ApplicationRecord
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
-    devise :database_authenticatable, :registerable,
+    devise :database_authenticatable, # :registerable,
            :recoverable, :rememberable, :trackable, :validatable
   end
 end
