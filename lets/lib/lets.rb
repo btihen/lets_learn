cefd5ea4cf74967e1532df886a59c1bf4d2ee6d8
Bill Tihen <btihen@gmail.com>
2018-05-16 22:10:32 +0200

    allow devise routing and customize login page

--- lets/lib/lets.rb
+++ lets/lib/lets.rb
@@ -1,6 +1,7 @@
-require "lets/engine"
-require 'bootstrap'
+# must require devise first to use local views instead of default devise views
 require 'devise'
+require 'bootstrap'
+require "lets/engine"
 
 module Lets
   # Your code goes here...
