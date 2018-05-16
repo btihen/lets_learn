14e738f75ab06c86d1e7f9b8216fff0fa822e789
Bill Tihen <btihen@gmail.com>
2018-05-16 21:00:34 +0200

    Installer ideas - need to play more

---
+++ lets/lib/generators/lets/install_generator.rb
@@ -0,0 +1,2 @@
+# https://github.com/SpinaCMS/Spina/blob/master/lib/generators/spina/install_generator.rb
+# https://stackoverflow.com/questions/39399016/add-migrations-to-a-rails-engines-install-generator?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
--- lets/lib/lets/engine.rb
+++ lets/lib/lets/engine.rb
@@ -3,14 +3,35 @@ module Lets
 
     isolate_namespace Lets
 
-    # set up for migrations
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match(root.to_s)
-        config.paths["db/migrate"].expanded.each do |p|
-          app.config.paths["db/migrate"] << p
-        end
+    # user setup:
+    #      rails generate lets:install
+    #      rails db:migrate
+    # or   rails lets:install:migrations
+    # https://www.amberbit.com/blog/2015/10/15/rails-mountable-engines/
+    initializer "lets", before: :load_config_initializers do |app|
+      # # add routing to core app
+      # # I think its easier to adjust in normal routes area
+      # Rails.application.routes.append do
+      #   mount Lets::Engine, at: "/data", as: 'lets'
+      #   # mount Lets::Engine => "/data", as: 'lets'
+      #   # mount Lets::Engine, at: "/data", as: 'lets'
+      # end
+
+      # add migrations to core app
+      config.paths["db/migrate"].expanded.each do |expanded_path|
+        Rails.application.config.paths["db/migrate"] << expanded_path
       end
     end
+
+    # # set up for migrations - from rails book
+    # https://content.pivotal.io/blog/leave-your-migrations-in-your-rails-engines
+    # initializer :append_migrations do |app|
+    #   unless app.root.to_s.match( root.to_s )
+    #     config.paths["db/migrate"].expanded.each do |expanded_path|
+    #       app.config.paths["db/migrate"] << expanded_path
+    #     end
+    #   end
+    # end
 
   end
 end
